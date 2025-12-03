64d2de047c061876287c90be004354dfd2b4660e
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-12-03 17:17:19 -0300

    criando conteudo principal da pagina company-fees

--- src/pages/CompanyFees/index.tsx
+++ src/pages/CompanyFees/index.tsx
@@ -1,7 +1,31 @@
+import { Container } from "../../components/container";
+
 export function CompanyFees() {
   return (
-    <div>
+    <Container>
       <h1>Página taxas para empresa</h1>
-    </div>
+      <form className="flex flex-col justify-center items-center border ">
+        <select name="bandeira" id="bandeira">
+          <option disabled selected>
+            Selecione a bandeira
+          </option>
+          <option value="visa">Visa</option>
+          <option value="mastercard">Mastercard</option>
+          <option value="elo">Elo</option>
+          <option value="hiper">Hiper</option>
+        </select>
+        <label>Valor</label>
+        <input type="text" placeholder="0" />
+        <label>Parcelas:</label>
+        <input type="text" placeholder="0" />
+
+        <p>Valor a receber: </p>
+        <div className="flex flex-row gap-2 ">
+          <button type="submit">Calcular</button>
+          <button>Limpar</button>
+        </div>
+      </form>
+      <p>Custo efetivo total</p>
+    </Container>
   );
 }
