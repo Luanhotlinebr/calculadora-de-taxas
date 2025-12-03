0d3c15ea354bfa02b2bc4093d78561e11800cc71
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-12-03 16:51:07 -0300

    implementando estilos na home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,10 +4,10 @@ export function Home() {
   return (
     <Container>
       <h1 className="text-2xl font-medium">Página home</h1>
-      <p>
+      <p className="mb-2">
         Olá! As taxas serão repassadas para o cliente ou assumidas pela empresa?
       </p>
-      <div className="flex flex-col">
+      <div className="flex flex-col ">
         <button className="bg-[#00af1d] text-white border p-3 mb-2 font-medium rounded-md cursor-pointer">
           Empresa
         </button>
