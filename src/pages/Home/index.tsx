f9f71118207046d07372f2b6f4522b9f77f6c009
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-12-03 15:07:34 -0300

    trabalhando na estrutura principal da página principal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,23 @@
+import { Container } from "../../components/container";
+
 export function Home() {
   return (
-    <div>
-      <h1>Página home</h1>
-    </div>
+    <Container>
+      <h1 className="text-2xl font-medium">Página home</h1>
+      <p>
+        Olá! As taxas serão repassadas para o cliente ou assumidas pela empresa?
+      </p>
+      <div className="flex flex-col">
+        <button className="bg-[#00af1d] text-white border p-3 mb-2 font-medium rounded-md cursor-pointer">
+          Empresa
+        </button>
+        <button className="bg-[#00af1d] text-white border p-3 mb-2 font-medium rounded-md cursor-pointer">
+          Cliente
+        </button>
+        <button className="bg-[#00af1d] text-white border p-3 mb-2 font-medium rounded-md cursor-pointer">
+          Cadastro de taxas
+        </button>
+      </div>
+    </Container>
   );
 }
