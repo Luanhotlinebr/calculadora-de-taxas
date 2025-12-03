4a95061fb224b424b7723cd32d3698c2bc8f763b
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-12-03 14:44:02 -0300

    cadastrando paginas principais da aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import { createBrowserRouter } from "react-router";
+import { ClientsFees } from "./pages/ClientsFees";
+import { CompanyFees } from "./pages/CompanyFees";
+import { FeeRegistration } from "./pages/FeeRegistration";
+import { Home } from "./pages/Home";
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/clients-fees", element: <ClientsFees /> },
+  { path: "/company-fees", element: <CompanyFees /> },
+  { path: "fee-registration", element: <FeeRegistration /> },
+]);
 
-function App() {
-  return <></>;
-}
-
-export default App;
+export { router };
---
+++ src/pages/FeeRegistration/index.tsx
@@ -0,0 +1,7 @@
+export function FeeRegistration() {
+  return (
+    <div>
+      <h1>Página cadastro de taxas</h1>
+    </div>
+  );
+}
