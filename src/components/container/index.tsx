dd1c9362d1a862c01b6962a48c6178d045d1e7c6
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-12-03 17:51:38 -0300

    ajustando novamente container so que na branch correta

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 export function Container({ children }: ContainerProps) {
   return (
-    <div className="w-full max-w-7xl mx-auto bg-white px-2 rounded-md border flex flex-col items-center justify-center min-h-full">
+    <div className="w-full max-w-7xl mx-auto bg-white px-2 rounded-md border flex flex-col items-center justify-center min-h-screen">
       {children}
     </div>
   );
